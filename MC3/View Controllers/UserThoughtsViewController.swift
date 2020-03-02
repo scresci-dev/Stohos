f4be063f12f95b8bbaf4a6b5c4a97d93a6647337
Silvio <silviocresci1996@gmail.com>
2020-03-02 10:42:01 +0100

    Commit

--- MC3/View Controllers/UserThoughtsViewController.swift
+++ MC3/View Controllers/UserThoughtsViewController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import CoreData
+import CoreMotion
 
 class UserThoughtsViewController: UIViewController {
     
@@ -16,6 +17,12 @@ class UserThoughtsViewController: UIViewController {
     var notEssentialThoughtCount: Int = 0
     var currentThought: String = " "
     var newThought: String = " "
+    
+    var timer = Timer()
+    
+    var randomNumbers : [Int] = [Int.random(in: 1...200),Int.random(in: 1...200),Int.random(in: 1...200),Int.random(in: 1...200),Int.random(in: 1...200),Int.random(in: 1...200),Int.random(in: 1...200),Int.random(in: 1...200),Int.random(in: 1...200),Int.random(in: 1...200)]
+    
+    var numThoughts: Int = 0
     
     let image1 = UIImage(named: "\(arrayCheck[0])")
     let image2 = UIImage(named: "\(arrayCheck[1])")
@@ -28,19 +35,93 @@ class UserThoughtsViewController: UIViewController {
     @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var treeImage: UIImageView!
+    @IBOutlet weak var Barriera: UIImageView!
+    @IBOutlet weak var BarrieraTop: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        essentialThoughtCount = essentialCount
-        notEssentialThoughtCount = notEssentialCount
-        editButton.setTitleColor(darkGrey, for: .normal)
-        deleteButton.setTitleColor(darkGrey, for: .normal)
-        thoughtView.textColor = lightGrey
-        allAnswer.append(currentThought)
-        displayTree()
-        displayMainView()
-    }
-    
+        var collision : UICollisionBehavior!
+        var animator : UIDynamicAnimator!
+         var gravity: UIGravityBehavior!
+        var motionManager = CMMotionManager()
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            essentialThoughtCount = essentialCount
+                  notEssentialThoughtCount = notEssentialCount
+                  editButton.setTitleColor(darkGrey, for: .normal)
+                  deleteButton.setTitleColor(darkGrey, for: .normal)
+                  thoughtView.textColor = lightGrey
+                  allAnswer.append(currentThought)
+                  displayTree()
+                  displayMainView()
+            // Do any additional setup after loading the view.
+            animator = UIDynamicAnimator(referenceView: view)
+            gravity = UIGravityBehavior(items: [])
+            gravity.gravityDirection = CGVector(dx: 0, dy: 0)
+            animator.addBehavior(gravity)
+            self.collision = UICollisionBehavior(items: [])
+            self.collision.translatesReferenceBoundsIntoBoundary = true
+            self.collision.addBoundary(withIdentifier: "Barrier1" as NSCopying, for: UIBezierPath(rect: Barriera.frame))
+            self.collision.addBoundary(withIdentifier: "Barrier2" as NSCopying, for: UIBezierPath(rect: BarrieraTop.frame))
+            self.animator.addBehavior(self.collision)
+//            DoppiaPalla.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(TextAppear)))
+            
+    //        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(changeImg(button: pensiero1, start:4)), userInfo: nil, repeats: true)
+//            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+//                if(self.randomNumbers[0] == 162){
+//                    self.randomNumbers[0] = 1
+//                }
+//                self.changeImg(button: self.pensiero1, n: self.randomNumbers[0], state: "essential")
+//                self.randomNumbers[0] += 1
+//            }
+//            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+//                if(self.randomNumbers[1] == 253){
+//                               self.randomNumbers[1] = 1
+//                           }
+//                self.changeImg(button: self.pensiero2, n: self.randomNumbers[1], state: "not essential")
+//                self.randomNumbers[1] += 1
+//            }
+//            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+//                if(self.randomNumbers[2] == 162){
+//                               self.randomNumbers[2] = 1
+//                           }
+//                self.changeImg(button: self.pensiero3, n: self.randomNumbers[2], state: "essential")
+//                self.randomNumbers[2] += 1
+//
+//            }
+//            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+//                if(self.randomNumbers[3] == 253){
+//                               self.randomNumbers[3] = 1
+//                           }
+//                self.changeImg(button: self.pensiero4, n: self.randomNumbers[3], state: "not essential")
+//                self.randomNumbers[3] += 1
+//
+//            }
+//            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+//                if(self.randomNumbers[4] == 162){
+//                               self.randomNumbers[4] = 1
+//                           }
+//                self.changeImg(button: self.pensiero5, n: self.randomNumbers[4], state: "essential")
+//                self.randomNumbers[4] += 1
+//
+//            }
+//            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+//                if(self.randomNumbers[5] == 162){
+//                               self.randomNumbers[5] = 1
+//                           }
+//                self.changeImg(button: self.pensiero6, n: self.randomNumbers[5], state: "essential")
+//                self.randomNumbers[5] += 1
+//
+//            }
+//            Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+//                if(self.randomNumbers[6] == 253){
+//                               self.randomNumbers[6] = 1
+//                           }
+//                self.changeImg(button: self.pensiero7, n: self.randomNumbers[6], state: "not essential")
+//                self.randomNumbers[6] += 1
+//
+//            }
+
+        }
+        
     func displayTree() {
         if (essentialThoughtCount > notEssentialThoughtCount) {
             treeImage.image = UIImage(named: "plant3")
