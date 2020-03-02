43045fbd209e31ed76fc70d13bc491b99e7ac05b
Gianna Stylianou <giannastylianou@gmail.com>
2020-03-02 23:49:50 +0100

    nothing important

--- MC3/View Controllers/WellDoneViewController.swift
+++ MC3/View Controllers/WellDoneViewController.swift
@@ -20,8 +20,10 @@ class WellDoneViewController: UIViewController {
         //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
         wellDoneText.textColor = lightGrey
         wellDoneText.font = UIFont(name: "NewYorkMedium-Regular", size: 18)
+        wellDoneText.frame = CGRect(x: 70.0, y: 400.0, width: 280.0, height: 150.0)
         wellDoneLabel.textColor = darkGrey
         wellDoneLabel.font = UIFont(name: "NewYorkMedium-Regular", size: 25)
+        wellDoneLabel.frame = CGRect(x: 100.0, y: 300.0, width: 200.0, height: 50.0)
         let gestureRec = UITapGestureRecognizer(target: self, action:  #selector (self.someAction (_:)))
         
         wellDoneView.addGestureRecognizer(gestureRec)
