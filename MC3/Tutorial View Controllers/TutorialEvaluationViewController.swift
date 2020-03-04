3320162794c1f655a4ff45bea8b8b1bf5bab4be0
Silvio <silviocresci1996@gmail.com>
2020-03-04 15:49:12 +0100

    Oooo

--- MC3/Tutorial View Controllers/TutorialEvaluationViewController.swift
+++ MC3/Tutorial View Controllers/TutorialEvaluationViewController.swift
@@ -21,6 +21,7 @@ class TutorialEvaluationViewController: UIViewController {
         let attributes = [NSAttributedString.Key.paragraphStyle: style]
         textAnswer.attributedText = NSAttributedString(string: textAnswer.text, attributes: attributes)
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         textAnswer.font = UIFont(name: "NewYorkMedium-Regular", size: 18)
         textAnswer.textAlignment = .center
@@ -30,10 +31,45 @@ class TutorialEvaluationViewController: UIViewController {
         nextButton.titleLabel?.font = UIFont(name: "NewYorkMedium-Regular", size: 25)
         nextButton.setTitleColor(buttonColor, for: .normal)
         nextButton.setTitle("Next", for: .normal)
+        
+        var j = 0
+        
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+           if(j >= 211){
+                j = 1
+            }
+            self.essentialButton.setImage(UIImage(named: "Rosa\(j)"), for: .normal)
+                j += 1
+                   }
+        
+        var p = 0
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+          if(p >= 211){
+               p = 1
+           }
+           self.notEssentialButton.setImage(UIImage(named: "Verde\(p)"), for: .normal)
+               p += 1
+                  }
+        var h = 0
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+                 if(h >= 211){
+                      h = 1
+                  }
+            self.labelView.image = UIImage(named: "Spento\(h)")
+                      h += 1
+                  }
     }
 
     @IBAction func essentialButton(_ sender: Any) {
         view.endEditing(true)
+        var h = 0
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+                 if(h >= 211){
+                      h = 1
+                  }
+            self.labelView.image = UIImage(named: "Rosa\(h)")
+                      h += 1
+                  }
         essentialCount += 1
         DatabaseHelper.istance.saveThoughtCoredata(text: userAnswer, evaluation: "essential")
         nextMove()
@@ -41,6 +77,14 @@ class TutorialEvaluationViewController: UIViewController {
 
     @IBAction func notEssentialButton(_ sender: Any) {
         view.endEditing(true)
+        var h = 0
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+                 if(h >= 211){
+                      h = 1
+                  }
+            self.labelView.image = UIImage(named: "Verde\(h)")
+                      h += 1
+                  }
         notEssentialCount += 1
         DatabaseHelper.istance.saveThoughtCoredata(text: userAnswer, evaluation: "not-essential")
         nextMove()
