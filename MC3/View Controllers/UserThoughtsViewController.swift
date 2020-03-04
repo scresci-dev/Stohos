7ece59cb19ca3c31c6f8d06747fcd25aa0fd9696
Silvio <silviocresci1996@gmail.com>
2020-03-04 18:18:38 +0100

    snks

--- MC3/View Controllers/UserThoughtsViewController.swift
+++ MC3/View Controllers/UserThoughtsViewController.swift
@@ -129,8 +129,6 @@ class UserThoughtsViewController: UIViewController {
                                       }
             }
             
-
-        
                   view.addSubview(SubView)
         }
         
