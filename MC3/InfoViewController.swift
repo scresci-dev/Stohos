b6f03a420ece91d73df10ed941ce8b4e121385d6
Gianna Stylianou <giannastylianou@gmail.com>
2020-03-11 22:05:20 +0200

    last oneee i swear

---
+++ MC3/InfoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InfoViewController.swift
+//  MC3
+//
+//  Created by Gianna Stylianou on 11/3/20.
+//  Copyright © 2020 Gianna Stylianou. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+    }
+    
+}
