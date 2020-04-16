76d147adf79df894612a2debfaa6d1bd982755ac
Silvio <silviocresci1996@gmail.com>
2020-04-16 19:51:59 +0200

    Onboarding

--- MC3/Database/MessagesDatabase.swift
+++ MC3/Database/MessagesDatabase.swift
@@ -22,7 +22,7 @@ let backgroundColor = UIColor(red: 0.92, green: 0.92, blue: 0.92, alpha: 1)
 let message1 = TutorialMessage(textMessage: "Welcome to Stohos", imageName: "Welcome")
 let message2 = TutorialMessage(textMessage: "According to Minimalism as a state of mind, a thought is Essential when it brings you joy and has a vital meaning for you", imageName: "Essential Description")
 let message3 = TutorialMessage(textMessage: "A thought is Non-Essential when it can lead you to focus only on the negative and waste valuable mental energy", imageName: "NonEssential Description")
-let message4 = TutorialMessage(textMessage: "Ready to start your journey of mind decluttering?", imageName: "Ready")
+let message4 = TutorialMessage(textMessage: "Ready to start your journey of mind decluttering?", imageName: "start your journey")
 
 var messageArray = [message1, message2, message3, message4]
 
--- MC3/Tutorial View Controllers/WelcomeViewController.swift
+++ MC3/Tutorial View Controllers/WelcomeViewController.swift
@@ -14,6 +14,9 @@ class WelcomeViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var buttonView: UIButton!
     @IBOutlet weak var launchscreenImg: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var start: UIButton!
+    @IBOutlet weak var startJourney: UIImageView!
     
     var counter: Int = 0
     var timerSpEss = Timer()
@@ -22,10 +25,20 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+       let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
         UserDefaults.standard.set(true, forKey: "Audio")
         overrideUserInterfaceStyle = .light
-        let gestureRec = UITapGestureRecognizer(target: self, action:  #selector (self.someAction (_:)))
-        view.addGestureRecognizer(gestureRec)
+//        let gestureRec = UITapGestureRecognizer(target: self, action:  #selector (self.someAction (_:)))
+//        view.addGestureRecognizer(gestureRec)
         showMessages()
     }
 
@@ -34,10 +47,27 @@ class WelcomeViewController: UIViewController {
         navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
-    @objc func someAction(_ sender:UITapGestureRecognizer) {
-        if(counter != 0){
-        counter += 1
-        showMessages()
+    @objc func respondToSwipeGesture(gesture: UIGestureRecognizer)
+    {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer
+        {
+            switch swipeGesture.direction
+            {
+            case UISwipeGestureRecognizer.Direction.right:
+                     //write your logic for right swipe
+                      if(counter != 0 && counter !=  1){
+                      counter -= 1
+                      showMessages()
+                      }
+            case UISwipeGestureRecognizer.Direction.left:
+                     //write your logic for left swipe
+                      if(counter != 0 && counter != 3){
+                      counter += 1
+                      showMessages()
+                      }
+                default:
+                    break
+            }
         }
     }
     
@@ -48,6 +78,11 @@ class WelcomeViewController: UIViewController {
         else {
             imageView.image = UIImage(named: "\(messageArray[counter].imageName)")
             if (counter == 1) {
+                start.isHidden = true
+                imageView.isHidden = false
+                startJourney.isHidden = true
+                self.pageControl.isHidden = false
+                pageControl.currentPage = 0
                 self.imageView.isHidden = false
                 self.launchscreenImg.isHidden = true
                 buttonView.isHidden = false
@@ -63,6 +98,10 @@ class WelcomeViewController: UIViewController {
                            }
             }
             else if (counter == 2) {
+                start.isHidden = true
+                imageView.isHidden = false
+                startJourney.isHidden = true
+                pageControl.currentPage = 1
                 buttonView.isHidden = false
                 var j = 1
                 timerSpEss.invalidate()
@@ -77,14 +116,16 @@ class WelcomeViewController: UIViewController {
             }
             else {
                 if(counter == 0){
+                    start.isHidden = true
+                    self.pageControl.isHidden = true
+                    startJourney.isHidden = true
                     self.imageView.isHidden = true
                     self.launchscreenImg.isHidden = false
                     var i = 0
                     buttonView.isHidden = true
                    timerLaunch = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { timer in
                         i += 1
-                        if(i<=100){
-                            print(i)
+                        if(i<=80){
                             self.launchscreenImg.image = UIImage(named: "launchscreen\(i)")
                         }
                         else{
@@ -102,9 +143,16 @@ class WelcomeViewController: UIViewController {
                         
                     }
                 }else{
+                    imageView.isHidden = true
+                    startJourney.isHidden = false
+                    start.isHidden = false
+                    pageControl.currentPage = 2
                 buttonView.isHidden = true
                 }
             }
         }
+    }
+    @IBAction func startPressed(_ sender: UIButton) {
+         performSegue(withIdentifier: "TutorialQuestion", sender: self)
     }
 }
